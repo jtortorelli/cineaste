618f383bf5299a6bdd8f52d8792902750d32db6b
James Tortorelli <James.Tortorelli@lifeway.com>
2016-02-06 16:10:51 -0600

    Revert Application controller to original state

--- app/controllers/Application.scala
+++ app/controllers/Application.scala
@@ -1,19 +1,11 @@
 package controllers
 
-import javax.inject.Inject
-
-import com.wizardsofsmart.cineaste.service.FilmService
-import play.api.libs.concurrent.Execution.Implicits.defaultContext
 import play.api.mvc._
 
+class Application extends Controller {
 
-class Application @Inject()(filmService: FilmService) extends Controller {
-
-  def index = Action.async {
-    val result = filmService.retrieveFilmsList
-    result.map { response =>
-      Ok(response.toString())
-    }
+  def index = Action {
+    Ok(views.html.index("Your new application is ready."))
   }
 
 }
