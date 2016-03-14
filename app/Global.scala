b12832d0184f4e807d5982a9ac1aff5914e8a612
James Tortorelli <James.Tortorelli@lifeway.com>
2016-03-14 08:18:40 -0500

    Delete unused global

--- app/Global.scala
+++
@@ -1,8 +0,0 @@
-package com.wizardofsmart.cineaste
-
-import play.api.GlobalSettings
-
-object Global extends GlobalSettings {
-  val neo4jEndpoint: String = "http://localhost:7474/db/data/transaction/commit"
-
-}
