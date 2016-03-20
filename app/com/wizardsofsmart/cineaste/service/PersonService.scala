16e11d9b570145ad7cece1a900d8986974bbd17b
James Tortorelli <James.Tortorelli@lifeway.com>
2016-03-19 22:17:52 -0500

    Stub person service

---
+++ app/com/wizardsofsmart/cineaste/service/PersonService.scala
@@ -0,0 +1,8 @@
+package com.wizardsofsmart.cineaste.service
+
+class PersonService {
+   def people = {
+      "This is a list of people"
+   }
+
+}
--- app/controllers/PersonController.scala
+++ app/controllers/PersonController.scala
@@ -1,11 +1,14 @@
 package controllers
 
+import javax.inject.Inject
+
+import com.wizardsofsmart.cineaste.service.PersonService
 import play.api.mvc.{Action, Controller}
 
-class PersonController extends Controller {
+class PersonController @Inject()(personService: PersonService) extends Controller {
 
    def people = Action {
-      Ok("This is the people page")
+      Ok(personService.people)
    }
 
 }
