9e29e00b1a92677ce69cc18b0d2cd63aa8ccba80
James Tortorelli <James.Tortorelli@lifeway.com>
2016-03-21 09:49:57 -0500

    Remove injected film service from application controller

--- app/controllers/Application.scala
+++ app/controllers/Application.scala
@@ -1,12 +1,9 @@
 package controllers
 
-import javax.inject.Inject
-
-import com.wizardsofsmart.cineaste.service.FilmService
 import play.api.mvc._
 
 
-class Application @Inject()(filmService: FilmService) extends Controller {
+class Application extends Controller {
 
    def index = Action {
       Ok(views.html.home())
