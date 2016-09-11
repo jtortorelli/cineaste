0eb90c1b01a5d94f8ab8d71239ff78095b452166
James Tortorelli <James.Tortorelli@lifeway.com>
2016-09-11 14:14:27 -0500

    Misc reformat

--- app/controllers/FilmController.scala
+++ app/controllers/FilmController.scala
@@ -11,25 +11,25 @@ import scala.concurrent.ExecutionContext.Implicits.global
 
 class FilmController @Inject()(filmService: FilmService) extends Controller {
 
-   def films = Action.async {
-      filmService.films.map {
-         case Right(response) => Ok(views.html.film.films(response))
-         case Left(error) => error match {
-            case e: Neo4jConnectionError => ServiceUnavailable(views.html.errors.serviceUnavailable(e.message))
-            case _ => InternalServerError(views.html.errors.internalServerError())
-         }
+  def films = Action.async {
+    filmService.films.map {
+      case Right(response) => Ok(views.html.film.films(response))
+      case Left(error) => error match {
+        case e: Neo4jConnectionError => ServiceUnavailable(views.html.errors.serviceUnavailable(e.message))
+        case _ => InternalServerError(views.html.errors.internalServerError())
       }
-   }
+    }
+  }
 
-   def film(uuid: String) = Action.async {
-      filmService.film(uuid).map {
-         case Right(response) => Ok(views.html.film.film(response._1, response._2, response._3, response._4, response._5, response._6, response._7))
-         case Left(error) => error match {
-            case e: Neo4jConnectionError => ServiceUnavailable(views.html.errors.serviceUnavailable(e.message))
-            case e: EmptyResultsError => NotFound(views.html.errors.notFound(e.message))
-            case _ => InternalServerError(views.html.errors.internalServerError())
-         }
+  def film(uuid: String) = Action.async {
+    filmService.film(uuid).map {
+      case Right(response) => Ok(views.html.film.film(response._1, response._2, response._3, response._4, response._5, response._6, response._7))
+      case Left(error) => error match {
+        case e: Neo4jConnectionError => ServiceUnavailable(views.html.errors.serviceUnavailable(e.message))
+        case e: EmptyResultsError => NotFound(views.html.errors.notFound(e.message))
+        case _ => InternalServerError(views.html.errors.internalServerError())
       }
-   }
+    }
+  }
 
 }
